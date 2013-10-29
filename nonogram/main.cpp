bb70b35ec366ad9e92b5a8b9e7894f2fbd1004b8
Nels Salminen <n.salminen@me.com>
2013-10-29 22:31:47 +0100

    Initial project commit.
    This commit contains the basic Netbeans project files.

---
+++ nonogram/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: nelssalminen
+ *
+ * Created on October 29, 2013, 10:27 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
