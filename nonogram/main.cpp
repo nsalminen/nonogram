fcfdf0c2ebd5a8874ff479d7269658ae9b70b8a8
Nels Salminen <n.salminen@me.com>
2013-11-18 00:39:27 +0100

    Final commit
    Fixed and resolved all

--- nonogram/main.cpp
+++ nonogram/main.cpp
@@ -1,9 +1,22 @@
+//Authors: Leroy Klotz and Nels Numan
+//Stdnr: Leroy: 1414372, Nels: 1459929
+//File: KlotzNuman.cpp
+//Description:
+//Dit programma biedt de mogelijkheid om een nonogram puzzel
+//te spelen.
+//Compiler: g++ 4.2.1 IDE: Netbeans, Mac OS X
+//Date: 15 november 2013
+
 #include <iostream>
 #include <fstream>
 #include <climits>
 #include <cstdlib>
 
 using namespace std;
+
+//De klasse Cursor bevat de coordinaten van de cursor op het speelveld. 
+//Het bestaan van deze klasse vereenvoudigt de interactie tussen de
+//cursor en het speelveld en zorgt voor ruimte voor uitbreiding van het programma
 
 class Cursor {
 private:
@@ -12,14 +25,18 @@ public:
     Cursor();
     int getX();
     int getY();
-    void setX(int x);
-    void setY(int y);
+    void centerCursor(int width, int height);
     void move(char direction, int width, int height);
 };
 
 Cursor::Cursor() {
     x = y = 0;
 }
+
+//Deze methode verandert de positie van de cursor op basis van parameters.
+//Deze methode heeft de parameters: direction;om de richting 
+//van de verplaatsing aan te geven, width en height; 
+//om de grootte van het speelveld aan te geven.
 
 void Cursor::move(char direction, int width, int height) {
     switch (direction) {
@@ -54,20 +71,23 @@ void Cursor::move(char direction, int width, int height) {
     }
 }
 
+//Een eenvoudige methode voor het opvragen van de x-positie van de Cursor
+
 int Cursor::getX() {
     return x;
 }
 
-void Cursor::setX(int x) {
-    this->x = x;
-}
+//Een eenvoudige methode voor het opvragen van de y-positie van de Cursor
 
 int Cursor::getY() {
     return y;
 }
 
-void Cursor::setY(int y) {
-    this->y = y;
+//Deze methode centreert de cursor op het speelveld
+
+void Cursor::centerCursor(int width, int height) {
+    x = width / 2;
+    y = height / 2;
 }
 
 class Nonogram {
@@ -75,10 +95,16 @@ private:
     const static int maxDimension = 50;
     int width, height;
     int percentage;
+    //Het percentage wat gebruikt wordt voor het willekeurig vullen
     bool grid[maxDimension][maxDimension];
+    //Het speelveld
     int horizontalValues[maxDimension][maxDimension];
+    //De horizontale beschrijvingen
     int verticalValues[maxDimension][maxDimension];
-    bool autoCount;
+    //De verticale beschrijvingen
+    bool autoCount; //Een boolean om aan te geven of de beschrijving...
+    //...automatisch moeten meetellen bij het veranderen 
+    //van de inhoud van het speelveld
 public:
     Cursor cursor;
     Nonogram();
@@ -89,7 +115,6 @@ public:
     void changePercentage();
     void changeSize();
     void toggle();
-    void submenu(char);
     void clean();
     void resetValues();
     bool emptyLine(int i);
@@ -102,19 +127,27 @@ public:
 };
 
 Nonogram::Nonogram() {
-    width = 20;
-    height = 10;
+    width = 15;
+    height = 15;
     percentage = 50;
     autoCount = true;
 }
+
+//Een eenvoudige methode voor het opvragen van de hoogte van het Nonogram
 
 int Nonogram::getHeight() {
     return height;
 }
 
+//Een eenvoudige methode voor het opvragen van de breedte van het Nonogram
+
 int Nonogram::getWidth() {
     return width;
 }
+
+//In deze methode worden het aantal X'en in een specifieke 
+//regel van het Nonogram geteld. Hierna wordt dit aantal 
+//vergeleken met de beschrijving.
 
 bool Nonogram::checkLineValue(int i, char orientation) {
     int counter = 0;
@@ -152,8 +185,6 @@ bool Nonogram::checkLineValue(int i, char orientation) {
                 }
             }
             for (int j = 0; j < height; j++) {
-                //cout << "tempValues[" << j << "]: " << tempValues[j] << endl;
-                //cout << "verticalValues[" << i << "][" << j << "]: " << verticalValues[i][j] << endl;
                 if (tempValues[j] != verticalValues[i][j]) {
                     return false;
                 }
@@ -163,6 +194,9 @@ bool Nonogram::checkLineValue(int i, char orientation) {
             return false;
     }
 }
+
+//Deze methode itereert door het volledige speelveld van het Nonogram 
+//en telt per regel het aantal X'en en schrijft dit naar de beschrijvingsarrays.
 
 void Nonogram::countValues() {
     resetValues();
@@ -202,6 +236,38 @@ void Nonogram::countValues() {
     }
 }
 
+//Verandert getallen van karakters naar integers
+
+int readNumber() {
+    int getal;
+    char temp;
+    temp = ' ';
+    getal = 0;
+
+    while (temp != '\n') {
+        temp = cin.get();
+        if (temp >= '0' && temp <= '9') {
+            getal = getal * 10 + (temp - '0');
+        }
+    }
+    return getal;
+}
+
+//Deze methode leest karakters in en geeft deze terug aan de functie
+//zonder overbodige enters
+
+void readSelection(char &output) {
+    output = cin.get();
+    char temp;
+    temp = output;
+    while (temp != '\n') {
+        temp = cin.get();
+    }
+}
+
+//Deze methode schrijft de huidige beschrijvingen en hoogte en breedte van het spel 
+//naar een bestand met een door de gebruiker gespecificeerde naam.
+
 void Nonogram::writeFile() {
     ofstream output;
     string outputFile;
@@ -230,6 +296,9 @@ void Nonogram::writeFile() {
     output.close();
 }
 
+//Deze methode leest de huidige beschrijvingen en hoogte en breedte van het spel 
+//van een bestand met een door de gebruiker gespecificeerde naam.
+
 void Nonogram::readFile() {
     ifstream input;
     string inputFile;
@@ -242,6 +311,7 @@ void Nonogram::readFile() {
     input.open(inputFile.c_str(), ios::in);
     if (input.fail()) {
         cout << "Error while loading file. Returning to main menu." << endl;
+        return;
     }
     input >> height;
     input.ignore(1);
@@ -250,6 +320,7 @@ void Nonogram::readFile() {
     clean();
     autoCount = false;
     resetValues();
+    cursor.centerCursor(width, height);
     while (!input.eof()) {
         input >> number;
         cout << "number: " << number << endl;
@@ -258,7 +329,6 @@ void Nonogram::readFile() {
                 horizontalValues[indexI][indexJ] = number;
             } else {
                 verticalValues[indexI][indexJ] = number;
-                cout << "Vertical number" << endl;
             }
             indexJ++;
             input.ignore(1);
@@ -275,6 +345,9 @@ void Nonogram::readFile() {
         }
     }
 }
+
+//Deze methode print het speelveld, beschrijvingen en V's op het scherm
+//van de gebruiker
 
 void Nonogram::print() {
     printHorizontalLine();
@@ -295,7 +368,7 @@ void Nonogram::print() {
             }
         }
         cout << " " << '+';
-        if (!autoCount) {
+        if (!autoCount) { //Het onderdeel waar de horizontale V's geprint worden
             if (checkLineValue(i, 'h')) {
                 cout << " " << 'V';
             } else {
@@ -303,6 +376,7 @@ void Nonogram::print() {
             }
         }
         j = 0;
+        //Het onderdeel waar de horizontale beschrijvingen geprint worden
         while (horizontalValues[i][j] > 0) {
             cout << " " << horizontalValues[i][j];
             j++;
@@ -310,7 +384,7 @@ void Nonogram::print() {
         cout << endl;
     }
     printHorizontalLine();
-    if (!autoCount) {
+    if (!autoCount) { //Het onderdeel waar de verticale V's geprint worden
         cout << ' ';
         for (i = 0; i < width; i++) {
             if (checkLineValue(i, 'v')) {
@@ -321,6 +395,7 @@ void Nonogram::print() {
         }
         cout << endl;
     }
+    //Het onderdeel waar de verticale beschrijvingen geprint worden
     for (i = 0; i < height; i++) {
         if (!emptyLine(i)) {
             cout << ' ';
@@ -338,12 +413,19 @@ void Nonogram::print() {
     }
 }
 
+//Deze methode is een hulpmethode voor Nonogram::print(). 
+//Er wordt met deze methode een horizontale lijn gelijk aan de
+//breedte van het spel met +-tekens op scherm geprint
+
 void Nonogram::printHorizontalLine() {
     for (int i = 0; i < width + 2; i++) {
         cout << '+' << ' ';
     }
     cout << endl;
 }
+
+//Deze methode is een hulpmethode voor Nonogram::print(). Er wordt met deze methode
+//nagegaan of een gehele regel van de onderste beschrijvingsarray leeg is
 
 bool Nonogram::emptyLine(int i) {
     for (int j = 0; j < width; j++) {
@@ -354,14 +436,28 @@ bool Nonogram::emptyLine(int i) {
     return true;
 }
 
+//Deze methode verandert de dimensies van het speelveld met behulp van de invoer van
+//de gebruiker
+
 void Nonogram::changeSize() {
-    cout << "Please enter width (< 50):" << endl;
-    cin >> width;
-    cout << "Please enter height (< 50):" << endl;
-    cin >> height;
+    int tempWidth = -1;
+    int tempHeight = -1;
+    while ((tempWidth > 50 || tempWidth < 0)) {
+        cout << "Please enter width (< 50):" << endl;
+        tempWidth = readNumber();
+    }
+    width = tempWidth;
+    while ((tempHeight > 50 || tempHeight < 0)) {
+        cout << "Please enter height (< 50):" << endl;
+        tempHeight = readNumber();
+    }
+    height = tempHeight;
     clean();
     resetValues();
+    cursor.centerCursor(width, height);
 }
+
+//Deze methode schakelt de toestand van een specifieke cel in het speelveld
 
 void Nonogram::toggle() {
     if (grid[cursor.getY()][cursor.getX()]) {
@@ -374,16 +470,29 @@ void Nonogram::toggle() {
     }
 }
 
+//Deze methode is een hulpmethode van Nonogram::fillRandomly(). 
+//Er wordt in deze methode een willekeurig getal gegenereerd.
+
 int Nonogram::getRandomNumber() {
     static int number = 42;
     number = ((221 * number + 1) % 1000) / 10;
     return number;
 }
 
+//Deze methode verandert het percentage gebruikt bij het 
+//willekeurig vullen van het speelveld met behulp van invoer van de gebruiker
+
 void Nonogram::changePercentage() {
-    cout << "Choose a percentage between 0 and 100:" << endl;
-    cin >> percentage;
+    int temp = -1;
+    while (temp < 0 || temp > 100) {
+        cout << "Choose a percentage between 0 and 100:" << endl;
+        temp = readNumber();
+    }
+    percentage = temp;
 }
+
+//Deze methode vult het speelveld op een willekeurige manier met behulp van het
+//gespecificeerde percentage
 
 void Nonogram::fillRandomly() {
     int number;
@@ -398,6 +507,9 @@ void Nonogram::fillRandomly() {
         }
     }
 }
+
+//Deze methode maakt de beschrijvingsarrays leeg, dat wil 
+//zeggen dat de twee arrays volledig gevuld worden met 0
 
 void Nonogram::resetValues() {
     int i = 0;
@@ -414,57 +526,75 @@ void Nonogram::resetValues() {
     }
 }
 
+//Deze methode maakt het speelveld leeg, dat wil zeggen dat de 
+//array van het speelveld volledig op false wordt gezet
+
 void Nonogram::clean() {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             grid[i][j] = false;
         }
     }
     autoCount = true;
+    cursor.centerCursor(width, height);
+    resetValues();
 }
 
-void Nonogram::submenu(char selection) {
-    while (selection != 'b') {
-        cout << "[S]change size | [P]change percentage | [M]back to mainmenu | [I]import | [E]export" << endl;
-        cin >> selection;
+//Deze methode kan vanuit het mainmenu aangeroepen en toont extra opties
+//op het scherm van de gebruiker
+
+void submenu(Nonogram &nono) {
+    char selection;
+    while (true) {
+        cout << "[S]change size | [P]change percentage | [I]import | [E]export | [M]mainmenu" << endl;
+        readSelection(selection);
         switch (char(tolower(selection))) {
             case 's':
-                changeSize();
+                nono.changeSize();
                 break;
             case 'p':
-                changePercentage();
+                nono.changePercentage();
                 break;
-            case 'r':
-                readFile();
+            case 'i':
+                nono.readFile();
                 break;
+            case 'e':
+                nono.writeFile();
             case 'm':
                 return;
-            case 'e':
-                writeFile();
         }
     }
 }
 
+//Deze methode print het logo van het programma op het scherm van de gebruiker
+
+void printLogo() {
+    cout << "      _   __" << endl;
+    cout << "     / | / /___  ____  ____  ____  _________  ____ ___" << endl;
+    cout << "    /  |/ / __ \\/ __ \\/ __ \\/ __ `/ ___/ __ `/ __ `__ \\ " << endl;
+    cout << "   / /|  / /_/ / / / / /_/ / /_/ / /  / /_/ / / / / / / " << endl;
+    cout << "  /_/ |_/\\____/_/ /_/\\____/\\__, /_/   \\__,_/_/ /_/ /_/ " << endl;
+    cout << "                          /____/ " << endl << endl;
+}
+
+//Deze methode toont de opties van het hoofdmenu aan de gebruiker
+//en verzoekt het Nonogram om zichzelf te printen
+
 void mainMenu() {
     char selection;
+    bool mainMenu = true;
     Nonogram nono;
     nono.clean();
     nono.resetValues();
-    while (selection != 'q' || selection != 'Q') {
-        cout << "      _   __" << endl;
-        cout << "     / | / /___  ____  ____  ____  _________  ____ ___" << endl;
-        cout << "    /  |/ / __ \\/ __ \\/ __ \\/ __ `/ ___/ __ `/ __ `__ \\ " << endl;
-        cout << "   / /|  / /_/ / / / / /_/ / /_/ / /  / /_/ / / / / / / " << endl;
-        cout << "  /_/ |_/\\____/_/ /_/\\____/\\__, /_/   \\__,_/_/ /_/ /_/ " << endl;
-        cout << "                          /____/ " << endl << endl;
+    while (mainMenu) {
+        printLogo();
         nono.print();
         cout << "[W]up | [A]left | [D]right | [S]down | [T]toggle node" << endl;
         cout << "[C]clean | [R]fill randomly | [M]more options | [Q]quit" << endl;
-        cin >> selection;
-
+        readSelection(selection);
         switch (char(tolower(selection))) {
             case 'm':
-                nono.submenu(selection);
+                submenu(nono);
                 break;
             case 'c':
                 nono.clean();
@@ -487,11 +617,26 @@ void mainMenu() {
             case 'd':
                 nono.cursor.move('r', nono.getWidth(), nono.getHeight());
                 break;
+            case 'q':
+                mainMenu = false;
         }
     }
 }
 
+//Deze methode print het 'infoblokje' van het programma
+
+void printInfo() {
+    cout << "Authors: Leroy Klotz and Nels Numan" << endl;
+    cout << "Stdnr: Leroy: 1414372, Nels: 1459929" << endl;
+    cout << "File: KlotzNuman.cc" << endl;
+    cout << "Description:" << endl;
+    cout << "Dit programma biedt de mogelijkheid om een nonogram puzzel te spelen" << endl;
+    cout << "Compiler: g++ 4.2.1 IDE: Netbeans, Mac OS X" << endl;
+    cout << "Date: 15 november 2013" << endl << endl;
+}
+
 int main() {
+    printInfo();
     mainMenu();
     return 0;
 }
